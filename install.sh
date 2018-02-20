806f1dfc07f23f8bf2f334dbb59bede8f34d66cb
Florian HULOT <FloW@test.local>
2018-02-20 15:40:54 +0100

    end bashfile

--- install.sh
+++ install.sh
@@ -2,30 +2,31 @@
 #Script for Ubuntu 17.10
 
 # Ansible
-sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install ansible -y
 
 # Nginx Nginx
-sudo apt-get install nginx
+sudo apt-get install nginx -y
 sudo rm /var/www/html/index.html
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
 sudo cp ./src/default /etc/nginx/sites-available/default
 
 # PHP 7.2
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update
-sudo apt install php7.2-fpm
-sudo apt install php7.2-curl php7.2-gd php7.2-mbstring php7.2-xmlrpc php7.2-xml php7.2-zip php7.2 php7.2-common php7.2-cli
-sudo systemctl restart php7.2-fpm
-sudo service php7.2-fpm status
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update -y
+sudo apt-get install php7.2-fpm -y
+sudo apt-get install php7.2-curl php7.2-gd php7.2-mbstring php7.2-xmlrpc php7.2-xml php7.2-zip php7.2 php7.2-common php7.2-cli -y
+sudo systemctl restart php7.2-fpm -y
 
 # Symphony 4
-sudo apt-get install composer
+sudo apt-get install composer -y
 
-# Central Server
-
-# Security Server
+# LXD
+sudo apt-get dist-upgrade -y
+sudo apt-get install lxd -y
+sudo apt-get update -y
+newgrp lxd
