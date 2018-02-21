0bf528de787d89f774110ad23087173a7625e86c
Florian HULOT <FloW@test.local>
2018-02-21 09:36:31 +0100

    ajout des sorties pour le script install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 #Script for Ubuntu 17.10
 
-# Ansible
+echo "[ANSIBLE] Installation..."
 sudo apt-get update -y
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install ansible -y
 
-# Nginx Nginx
+echo "[NGINX] Installation..."
 sudo apt-get install nginx -y
 sudo rm /var/www/html/index.html
+echo "[NGINX] Backup of the configuration file"
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+echo "[NGINX] Import of out configuration file"
 sudo cp ./src/default /etc/nginx/sites-available/default
+echo "[NGINX] Done."
 
-# PHP 7.2
+echo "[PHP7.2] Installation..."
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update -y
@@ -22,23 +25,28 @@ sudo apt install php7.2-fpm -y
 sudo apt install php7.2-curl php7.2-gd php7.2-mbstring php7.2-xmlrpc php7.2-xml php7.2-zip php7.2 php7.2-common php7.2-cli -y
 sudo systemctl restart php7.2-fpm
 sudo service php7.2-fpm status
+echo "[PHP7.2] Done."
 
-# Symphony 4
+echo "[SF4] Installation..."
 sudo apt-get install composer -y
 sudo cp -r road_installer /var/www/html
 sudo composer update -d=/var/www/html/road_installer
+echo "[SF4] Done."
 
-#git + git clone de X-road
+echo "[GIT] Installation..."
 sudo apt-get install git -y
+echo "[GIT] Done."
+
+echo "[XROAD] Git Clone on /etc/x-road-repo ..."
 sudo mkdir /etc/x-road-repo
 sudo chmod 777 /etc/x-road-repo
 sudo git clone "https://github.com/ria-ee/X-Road"
 sudo cp -r X-Road /etc/x-road-repo
+echo "[XROAD] Done."
 
-# Central Server
-
-# LXD
+echo "[LXD] Installation..."
 sudo apt-get dist-upgrade -y
 sudo apt-get install lxd -y
 sudo apt-get update -y
 newgrp lxd
+echo "[LXD] Done."
