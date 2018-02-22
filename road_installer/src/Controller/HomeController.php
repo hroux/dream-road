94558c7a7284ea7889ee73aa067150c86a25c646
Nileeda <nileeda@gmail.com>
2018-02-22 09:48:38 +0100

    Ajout de la base graphique du site

---
+++ road_installer/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function indexAction()
+    {
+        return $this->render('home.html.twig', array("test" => "je suis un test", "log" => "log"));
+    }
+}
